f2a0004a76693ce5ee383df3a42fb93e27b0724b
limuen <limuen_dev@163.com>
2024-06-18 14:34:39 +0800

    feat: 🚀 add plugins to the demonstration project

--- apps/defaultProject/vite.config.ts
+++ apps/defaultProject/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig, mergeConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import checker from "vite-plugin-checker";
 import viteConfig from "@limuen/viteconfig";
 
 export default defineConfig(configEnv => {
-  return mergeConfig(viteConfig(configEnv), {});
+  return mergeConfig(viteConfig(configEnv), {
+    plugins: [react(), checker({ typescript: true })]
+  });
 });
