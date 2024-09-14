a97cd3f6c45d85e54d8772bd27ba372e47085042
limuen <limuen_dev@163.com>
2024-09-14 13:53:54 +0800

    feat: 🚀 add codeInspectorPlugin to React low code repo

--- apps/react-low-code/vite.config.ts
+++ apps/react-low-code/vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig, mergeConfig } from "vite";
 import viteConfig from "@limuen/viteconfig";
+import { codeInspectorPlugin } from "code-inspector-plugin";
 
 export default defineConfig(configEnv => {
-  return mergeConfig(viteConfig(configEnv), {});
+  return mergeConfig(viteConfig(configEnv), {
+    plugins: [
+      codeInspectorPlugin({
+        bundler: "vite"
+      })
+    ]
+  });
 });
